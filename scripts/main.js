04244a0666ceef17103a42633950a5f85e3b3cff
brholland805 <144716844+brholland805@users.noreply.github.com>
2024-06-05 23:39:06 +0000

    save.html

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,11 @@
-console.log()
+// console.log()
+document.addEventListener("DOMContentLoaded", () => {
+    const contactForm = document.getElementById('contact-form');
+    contactForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        alert('Form submitted successfully!');
+    });
+
+    // Dynamic content creation example
+    if (document.getElementById('project-list')) {
+        const projects
